c501a6905dc6e6255bb4500da9848ff6292d42f7
TTBob <erik.k.lagerstedt@gmail.com>
2024-02-05 18:14:41 +0100

    Add sprint and stamina mechanics

--- Panic Jam Game Project/Assets/Scripts/PlayerController.cs
+++ Panic Jam Game Project/Assets/Scripts/PlayerController.cs
@@ -1,27 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour 
 {
-    public float topSpeed;
-    [Range(0, 1)]
-    public float acc;
+    [Header("Walking")]
+    [Tooltip("The speed of walking (meters per second)")] public float walkSpeed;
+    [Tooltip("The acceleration if velocity < walkspeed or no horizontal input is recieved")] public float walkAcc;
+
+    [Header("Sprinting")]
+    [Tooltip("Applied when shift is pressed")] public float sprintSpeed;
+    [Tooltip("Applied when velocity > walkSpeed and horizontal input is recieved")] public float sprintAcc;
+    
+    [Header("Stamina")]
+    [Tooltip("Time until stamina runs out (seconds)")] public float sprintTime;
+    private float stamina;
+    private bool staminaState; // True = usable, False = locked for refill
+    [Tooltip("Time for stamina to refill (seconds)")] public float refillTime;
+
     private float targetVel;
-    public float jumpForce;
-    private Rigidbody2D rb;
+    
+    [Header("Jump")]
+    [Tooltip("The force applied vertically to make the character jump, always applied once per jump")] public float jumpForce;
+
+    [Header("Objects")]
     public GameSettings gameSettings;
+    public Transform staminaUI;
+    private Rigidbody2D rb;
+    
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        stamina = 1;
     }
+    
     void FixedUpdate() {
-        targetVel = Input.GetAxisRaw("Horizontal") * topSpeed;
-        float vel = Mathf.Lerp(rb.velocity.x, targetVel, 1 - Mathf.Pow(1 - acc, Time.deltaTime * gameSettings.targetFPS));
+        bool sprinting = Input.GetButton("Sprint") && staminaState;
+        targetVel = Input.GetAxisRaw("Horizontal") * (sprinting ? sprintSpeed : walkSpeed);
+
+        // Calculate acceleration and stamina change
+        float acc;
+        // If sprinting, velocity > walkspeed and input in same direction as velocity (check input to make stopping completely and turning around after running faster)
+        if (sprinting && ((rb.velocity.x > walkSpeed && Input.GetAxisRaw("Horizontal") == 1) || (rb.velocity.x < -walkSpeed && Input.GetAxisRaw("Horizontal") == -1))) {
+            // Using stamina:
+            acc = sprintAcc;
+            stamina = Mathf.MoveTowards(stamina, 0, Time.deltaTime / sprintTime);
+            if (stamina == 0) {
+                staminaState = false;
+            }
+        } else {
+            // Not using stamina:
+            acc = walkAcc;
+            stamina = Mathf.MoveTowards(stamina, 1, Time.deltaTime / refillTime);
+            if (stamina == 1) {
+                staminaState = true;
+            }
+        }
+
+        // Calculate velocity and apply to rigidbody
+        float vel = Mathf.MoveTowards(rb.velocity.x, targetVel, acc * Time.deltaTime);
         rb.velocity = new Vector2(vel, rb.velocity.y);
 
+        // Update the stamina UI element (Temporary)
+        float UIPos = Mathf.Lerp(-1, 0, stamina);
+        float UIWidth = Mathf.Lerp(0, 2, stamina);
+        staminaUI.localPosition = new Vector3(UIPos, staminaUI.localPosition.y, 0);
+        staminaUI.localScale = new Vector3(UIWidth, staminaUI.localScale.y, 1);
+        staminaUI.gameObject.GetComponent<SpriteRenderer>().color = staminaState ? Color.yellow : Color.red;
+
+        // Jump and ground check
         if (Input.GetButton("Jump")) {
             RaycastHit2D hit;
             hit = Physics2D.BoxCast(transform.position, Vector2.one, 0, -Vector2.up, 0.51f);
